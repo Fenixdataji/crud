f8911aaf376acc9f64079e60624c697d8dc9deec
fenixdataji <dataji@gmail.com>
2021-02-14 15:33:14 -0500

    Formulario

--- src/App.js
+++ src/App.js
@@ -6,7 +6,25 @@ function App() {
     <div className="container mt-5">
         <h1>Tareas</h1>
         <hr/>
-
+        <div className="row">
+            <div className ="col-8"> 
+              <h4 className="text-center">Lista de Tareas</h4>
+              <ul className="list-group">
+                <li className="list-group-item">
+                  <span className="lead">Nombre de la Tarea</span>
+                  <button className="btn btn-danger btn-sm float-right mx-2">Eliminar</button>
+                  <button className="btn btn-warning btn-sm float-right">Editar</button>
+                </li>
+              </ul>
+            </div>
+            <div className ="col-4"> 
+            <h4 className="text-center">Formulario</h4>
+            <form>
+              <input type="text" className="form-control mb-2" placeholder="Ingrese la Tarea...."/>
+              <button className="btn btn-dark btn-block" type="submit">Agregar</button>
+            </form>
+            </div>
+        </div>
     </div>
   )
 }
